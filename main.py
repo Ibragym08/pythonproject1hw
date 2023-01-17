10ed966db2113ca75c800daf4a73e98497acd558
Aliev_ox53 <i6ra.gym@mail.ru>
2023-01-17 17:54:29 +0400

    commit

---
+++ main.py
@@ -0,0 +1,42 @@
+#Task 1
+# user_inp = input()
+#
+# split = user_inp.split()
+#
+#
+# if "+" in user_inp:
+#     split1 = user_inp.split("+")
+#     num1 = int(split1[0])
+#     num2 = int(split1[1])
+#     numbers = num1 + num2
+#     print(numbers)
+# if "-" in user_inp:
+#     split1 = user_inp.split("-")
+#     num1 = int(split1[0])
+#     num2 = int(split1[1])
+#     numbers = num1 - num2
+#     print(numbers)
+# if "*" in user_inp:
+#     split1 = user_inp.split("*")
+#     num1 = int(split1[0])
+#     num2 = int(split1[1])
+#     numbers = num1 * num2
+#     print(numbers)
+# if "/" in user_inp:
+#     split1 = user_inp.split("/")
+#     num1 = int(split1[0])
+#     num2 = int(split1[1])
+#     numbers = num1 / num2
+#     print(numbers)
+#Task 2
+# user_inp = input()
+# split = user_inp.split(".")
+# print(split)
+# str_cap = ""
+# for c in split:
+#     str_cap += f"{c.capitalize()}. "
+#
+# print(str_cap)
+# #
+# # ""join.(c.capitalize())
+#
